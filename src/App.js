23caa57c9611f6a7da47107c32e3828d73189bd1
joldon <79482239+Joldon@users.noreply.github.com>
2023-01-09 20:40:15 +0100

    styling of global map and implementing a reduce function

--- src/App.js
+++ src/App.js
@@ -1,17 +1,44 @@
 import React from "react";
 import "./App.css";
 import { ComposableMap, Geographies, Geography } from "react-simple-maps";
+import { scaleLinear } from "d3-scale";
+import conflictsData from "./data/ucdp.json";
 
 function App() {
-  // const geoUrl = "./world-110m.json";
-  // const geoUrl = "https://observablehq.com/@d3/world-map-svg";
-
-  // const geoUrl =
-  //   "https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-110m.json";
   const geoUrl =
     "https://raw.githubusercontent.com/deldersveld/topojson/master/world-countries.json";
 
-  console.log(geoUrl);
+  const groupObjectByProperty = (objArray, property) => {
+    return objArray.reduce((acc, obj) => {
+      const key = obj[property];
+      if (!acc[key]) {
+        return {
+          ...acc,
+          [key]: {
+            conflicts: 1,
+            fatalities: obj.best,
+            countryName: obj.country,
+            type: obj.type_of_violence,
+          },
+        };
+      }
+
+      return {
+        ...acc,
+        [key]: {
+          ...acc[key],
+          conflicts: acc[key].conflicts + 1,
+        },
+      };
+    }, {});
+  };
+
+  // const conflictsByCountry = groupObjectByProperty(conflictsData, "country");
+  // console.log(conflictsByCountry);
+
+  const colorScale = scaleLinear()
+    .domain([0, 100])
+    .range(["#FFF", "#06F"]);
 
   return (
     <>
@@ -26,7 +53,21 @@ function App() {
         <Geographies geography={geoUrl}>
           {({ geographies }) =>
             geographies.map((geo) => (
-              <Geography key={geo.rsmKey} geography={geo} />
+              <Geography
+                key={geo.rsmKey}
+                geography={geo}
+                style={{
+                  default: {
+                    fill: "#EEE",
+                  },
+                  hover: {
+                    fill: "#F53",
+                  },
+                  pressed: {
+                    fill: "#E42",
+                  },
+                }}
+              />
             ))
           }
         </Geographies>
