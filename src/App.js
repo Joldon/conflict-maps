e00f5ea96851ed949e663007c42a9a25aead7092
joldon <79482239+Joldon@users.noreply.github.com>
2023-01-09 11:09:29 +0100

    rendered topojson map

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
+import React from "react";
 import "./App.css";
+import { ComposableMap, Geographies, Geography } from "react-simple-maps";
 
 function App() {
+  // const geoUrl = "./world-110m.json";
+  // const geoUrl = "https://observablehq.com/@d3/world-map-svg";
+
+  // const geoUrl =
+  //   "https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-110m.json";
+  const geoUrl =
+    "https://raw.githubusercontent.com/deldersveld/topojson/master/world-countries.json";
+
+  console.log(geoUrl);
+
   return (
-    <div className="App">
+    <>
       <p>Conflict Map</p>
-    </div>
+      <ComposableMap
+        projection="geoEqualEarth"
+        projectionConfig={{
+          rotate: [-10, 0, 0],
+          scale: 170,
+        }}
+      >
+        <Geographies geography={geoUrl}>
+          {({ geographies }) =>
+            geographies.map((geo) => (
+              <Geography key={geo.rsmKey} geography={geo} />
+            ))
+          }
+        </Geographies>
+      </ComposableMap>
+    </>
   );
 }
 
